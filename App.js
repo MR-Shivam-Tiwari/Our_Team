7172df9d09d9eb70bce3341a03292f563fd313e6
shivam <shivamt2023@gmail.com>
2023-05-11 11:15:47 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,266 @@
+import { Tab, Tabs } from "react-tabs";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="meet">Meet Our Team</h1>
+      <p className="para">
+        Meet our diverse team of world-class Creators,Designers,Developers and
+        problem solvers
+      </p>
+      <div className="tab">
+        <Tabs className="sadr">
+          <Tab>View all</Tab>
+          <Tab>Management</Tab>
+          <Tab>Product</Tab>
+          <Tab>Designer</Tab>
+          <Tab>Marketing</Tab>
+          <Tab>Developers</Tab>
+        </Tabs>
+      </div>
+      <div class="parent">
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/sakshi.webp"
+            alt="Logo"
+          />
+          <div className="round">
+            <p class="text-blk ">Sakshi Verma</p>
+            <p class="position">Operations Head</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/saurabh.webp"
+            alt="Logo"
+          />
+          <div className="round">
+            <p class="text-blk ">Saurabh Sharma</p>
+            <p class="position">Software Developer</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/santosh.webp"
+            alt="Logo"
+          />
+          <div className="round">
+            <p class="text-blk ">Santosh Kushwaha</p>
+            <p class="position">Software Developer</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/garg.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Mr. Garg</p>
+            <p class="position">DevOps Engineer</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="parent">
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/eshanika.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Eshanika Ray</p>
+            <p class="position">Content and Editing</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/rohit.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Rohit Dabaas</p>
+            <p class="position">Graphic Design</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/ayush.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Ayush Antiwal</p>
+            <p class="position">Web Developer</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/rishabh.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Rishabh Jain</p>
+            <p class="position">Web Developer</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="parent">
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/roop.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Roop Kishor</p>
+            <p class="position">Web Developer</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/Prakhar-Varshney.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Prakhar</p>
+            <p class="position">State Coordinator and PR</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/Prashant-Yadav.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Prashant Yadav</p>
+            <p class="position">State Coordinator and PR</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/Gaurav-Baghel.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Gaurav</p>
+            <p class="position">State Coordinator</p>
+          </div>
+        </div>
+      </div>
+      <div class="parent">
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/Durga-Prasad-Mishra.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Durga Prasad Mishra</p>
+            <p class="position">Content writer</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/Pooja-Vishwakarma.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Pooja Vishwakarma</p>
+            <p class="position">Graphic Designer</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/Janvi-sethi.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Janvi</p>
+            <p class="position">Graphic Designer</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/Aarya-Singh-Parihar.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Aarya</p>
+            <p class="position">Coordinator</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="parent">
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/Abhishek-Chaurasiya.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Abhishek</p>
+            <p class="position">Coordinator</p>
+          </div>
+        </div>
+        <div class="img-wrapper">
+          <img
+            class="team-img"
+            src="https://glcloud.in/images/static/team/Khushboo-Jha.webp"
+            alt="Logo"
+          />{" "}
+          <div className="round">
+            <p class="text-blk ">Khushboo</p>
+            <p class="position">Coordinator</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="box">
+        <h1 className="boxh1">
+          {" "}
+          Untitled manages our complete hiring process super efficiently and
+          effectively. It's a competitive edge.
+        </h1>
+        <img
+          className="author"
+          src="https://img.freepik.com/free-photo/amazing-cheerful-business-woman-standing-with-arms-crossed_171337-8487.jpg"
+          alt="Girl in a jacket"
+          width="50"
+          height="45"
+        />
+        <h2>Millie Bourke</h2>
+        <h5> Founder</h5>
+      </div>
+      <div className="box2">
+        <h1>We're hiring</h1>
+        <p>We're always looking for great people.Chat to our team.</p>
+
+        <button>Explore open roles</button>
+      </div>
+
+      <div className="box3">
+        <h1>Hiring fast? We can help.</h1>
+        <p>Start your 30-day free trial. Cancel anytime</p>
+        <button>Get Started</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
